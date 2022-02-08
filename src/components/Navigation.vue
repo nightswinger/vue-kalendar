5e620fabcfff71da58ef78e5ae3441fac6b57987
nightswinger <stardustkids83@gmail.com>
2022-02-08 15:00:37 +0000

    add Navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">import { computed } from 'vue';
+
+const props = defineProps<{
+  activeStartDate: Date
+  view: string
+}>()
+
+const formatMonthYear = (date: Date) => Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(date)
+const label = computed(() => {
+  switch (props.view) {
+    case 'month':
+      return formatMonthYear(props.activeStartDate)
+    default:
+      throw new Error(`Invalid view: ${props.view}`)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <button>«</button>
+    <button>‹</button>
+    <button>
+      <span>
+        {{ label }}
+      </span>
+    </button>
+    <button>›</button>
+    <button>»</button>
+  </div>
+</template>
